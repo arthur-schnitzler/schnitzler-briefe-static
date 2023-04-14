3efcb2cc4404588615b1435e0a64063c74033b18
Laura Untner <83955990+laurauntner@users.noreply.github.com>
2023-04-14 11:21:10 +0200

    fix

--- fetch_data.sh
+++ fetch_data.sh
@@ -15,7 +15,7 @@ find ./data/editions/ -type f -name "*.xml"  -print0 | xargs -0 sed -i -e 's@xsi
 echo "fixing entity ids"
 find ./data/indices/ -type f -name "*.xml"  -print0 | xargs -0 sed -i -e 's@<person xml:id="person__@<person xml:id="pmb@g'
 
-rm -rf data
+rm -rf pdf-leseansicht
 wget https://github.com/arthur-schnitzler/schnitzler-briefe-tex/archive/refs/heads/main.zip
 unzip main
 
