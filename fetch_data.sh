1d321ef1c9841560b55a53b3419a2a6f2558f60e
Laura Untner <83955990+laurauntner@users.noreply.github.com>
2023-04-14 10:52:20 +0200

    update fetch_data.sh to get pdf files

--- fetch_data.sh
+++ fetch_data.sh
@@ -14,3 +14,11 @@ find ./data/editions/ -type f -name "*.xml"  -print0 | xargs -0 sed -i -e 's@xsi
 
 echo "fixing entity ids"
 find ./data/indices/ -type f -name "*.xml"  -print0 | xargs -0 sed -i -e 's@<person xml:id="person__@<person xml:id="pmb@g'
+
+rm -rf data
+wget https://github.com/arthur-schnitzler/schnitzler-briefe-tex/archive/refs/heads/main.zip
+unzip main
+
+mv ./schnitzler-briefe-tex-main/pdf-leseansicht .
+rm main.zip
+rm -rf ./schnitzler-briefe-tex-main
