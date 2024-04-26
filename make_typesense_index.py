3cc9766d5906dcbb80606675287549af810ae212
csae8092 <peter.andorfer@oeaw.ac.at>
2024-04-26 10:44:42 +0200

    wip #74

--- make_typesense_index.py
+++ make_typesense_index.py
@@ -5,67 +5,76 @@ from typesense.api_call import ObjectNotFound
 from acdh_cfts_pyutils import TYPESENSE_CLIENT as client
 from acdh_cfts_pyutils import CFTS_COLLECTION
 from acdh_tei_pyutils.tei import TeiReader
+from acdh_tei_pyutils.utils import (
+    extract_fulltext,
+    get_xmlid,
+    make_entity_label,
+    check_for_hash,
+)
 from tqdm import tqdm
 
 
-files = glob.glob('./data/editions/*.xml')
+files = glob.glob("./data/editions/*.xml")
+COLLECTION_NAME = "schnitzler-briefe"
+MIN_DATE = "1000"
 
 
 try:
-    client.collections['schnitzler-briefe'].delete()
+    client.collections[COLLECTION_NAME].delete()
 except ObjectNotFound:
     pass
 
 current_schema = {
-    'name': 'schnitzler-briefe',
-    'fields': [
+    "name": COLLECTION_NAME,
+    "enable_nested_fields": True,
+    "fields": [
+        {"name": "id", "type": "string"},
+        {"name": "rec_id", "type": "string"},
+        {"name": "title", "type": "string"},
+        {"name": "full_text", "type": "string"},
         {
-            'name': 'id',
-            'type': 'string'
+            "name": "year",
+            "type": "int32",
+            "optional": True,
+            "facet": True,
         },
         {
-            'name': 'rec_id',
-            'type': 'string'
+            "name": "sender",
+            "type": "object[]",
+            "facet": True,
+            "optional": True,
         },
         {
-            'name': 'title',
-            'type': 'string'
+            "name": "receiver",
+            "type": "object[]",
+            "facet": True,
+            "optional": True,
         },
         {
-            'name': 'full_text',
-            'type': 'string'
+            "name": "persons",
+            "type": "object[]",
+            "facet": True,
+            "optional": True,
         },
         {
-            'name': 'year',
-            'type': 'int32',
-            'optional': True,
-            'facet': True,
+            "name": "places",
+            "type": "object[]",
+            "facet": True,
+            "optional": True,
         },
         {
-            'name': 'persons',
-            'type': 'string[]',
-            'facet': True,
-            'optional': True
+            "name": "orgs",
+            "type": "object[]",
+            "facet": True,
+            "optional": True,
         },
         {
-            'name': 'places',
-            'type': 'string[]',
-            'facet': True,
-            'optional': True
+            "name": "works",
+            "type": "object[]",
+            "facet": True,
+            "optional": True,
         },
-        {
-            'name': 'orgs',
-            'type': 'string[]',
-            'facet': True,
-            'optional': True
-        },
-        {
-            'name': 'works',
-            'type': 'string[]',
-            'facet': True,
-            'optional': True
-        },
-    ]
+    ],
 }
 
 client.collections.create(current_schema)
@@ -74,53 +83,101 @@ records = []
 cfts_records = []
 for x in tqdm(files, total=len(files)):
     cfts_record = {
-        'project': 'schnitzler-briefe',
+        "project": COLLECTION_NAME,
     }
     record = {}
     doc = TeiReader(x)
-    body = doc.any_xpath('.//tei:body')[0]
-    record['id'] = os.path.split(x)[-1].replace('.xml', '')
-    cfts_record['id'] = record['id']
-    cfts_record['resolver'] = f"https://schnitzler-briefe.acdh.oeaw.ac.at/{record['id']}.html"
-    record['rec_id'] = os.path.split(x)[-1]
-    cfts_record['rec_id'] = record['rec_id']
-    record['title'] = " ".join(" ".join(doc.any_xpath('.//tei:titleStmt/tei:title[@level="a"]/text()')).split())
-    cfts_record['title'] = record['title']
+    body = doc.any_xpath(".//tei:body")[0]
+    record["id"] = os.path.split(x)[-1].replace(".xml", "")
+    cfts_record["id"] = record["id"]
+    cfts_record["resolver"] = (
+        f"https://schnitzler-briefe.acdh.oeaw.ac.at/{record['id']}.html"
+    )
+    record["rec_id"] = os.path.split(x)[-1]
+    cfts_record["rec_id"] = record["rec_id"]
+    record["title"] = extract_fulltext(
+        doc.any_xpath('.//tei:titleStmt/tei:title[@level="a"]')[0]
+    )
+    cfts_record["title"] = record["title"]
     try:
-        date_str = doc.any_xpath('//tei:titleStmt/tei:title[@type="iso-date"]/@when-iso')[0]
+        date_str = doc.any_xpath(
+            '//tei:titleStmt/tei:title[@type="iso-date"]/@when-iso'
+        )[0]
     except IndexError:
-        date_str = "1000"
+        date_str = MIN_DATE
+    
+    record["sender"] = []
+    try:
+        sender_label = doc.any_xpath(
+            './/tei:correspAction[@type="sent"]/tei:persName/text()'
+        )[0]
+        sender_id = check_for_hash(doc.any_xpath(
+            './/tei:correspAction[@type="sent"]/tei:persName/@ref'
+        )[0])
+    except Exception as e:
+        print(f"sender issues in {x}, due to: {e}")
+        sender_label = "Kein Absender"
+        sender_id = None
+    record["sender"].append({
+        "label": sender_label,
+        "id": sender_id
+    })
+    record["receiver"] = []
+    try:
+        receiver_label = doc.any_xpath(
+            './/tei:correspAction[@type="received"]/tei:persName/text()'
+        )[0]
+        receiver_id = check_for_hash(doc.any_xpath(
+            './/tei:correspAction[@type="received"]/tei:persName/@ref'
+        )[0])
+    except Exception as e:
+        print(f"receiver issues in {x}, due to: {e}")
+        receiver_label = "Kein Absender"
+        receiver_id = None
+    record["receiver"].append({
+        "label": receiver_label,
+        "id": receiver_id
+    })
 
     try:
-        record['year'] = int(date_str[:4])
-        cfts_record['year'] = int(date_str[:4])
+        record["year"] = int(date_str[:4])
+        cfts_record["year"] = int(date_str[:4])
     except ValueError:
         pass
-    record['persons'] = [
-        " ".join(" ".join(x.xpath('.//text()')).split()) for x in doc.any_xpath('.//tei:back//tei:person/tei:persName[1]')
-    ]
-    cfts_record['persons'] = record['persons']
-    record['places'] = [
-         " ".join(" ".join(x.xpath('.//text()')).split()) for x in doc.any_xpath('.//tei:back//tei:place[@xml:id]/tei:placeName[1]')
-    ]
-    cfts_record['places'] = record['places']
-    record['orgs'] = [
-         " ".join(" ".join(x.xpath('.//text()')).split()) for x in doc.any_xpath('.//tei:back//tei:org[@xml:id]/tei:orgName[1]')
-    ]
-    cfts_record['orgs'] = record['orgs']
-    record['works'] = [
-         " ".join(" ".join(x.xpath('.//text()')).split()) for x in doc.any_xpath('.//tei:back//tei:listBibl//tei:bibl[@xml:id]/tei:title[1]')
-    ]
-    cfts_record['works'] = record['works']
-    record['full_text'] = " ".join(''.join(body.itertext()).split())
-    cfts_record['full_text'] = record['full_text']
+    record["persons"] = []
+    for y in doc.any_xpath(".//tei:back//tei:person[@xml:id]"):
+        item = {"id": get_xmlid(y), "label": make_entity_label(y.xpath("./*[1]")[0])[0]}
+        record["persons"].append(item)
+    cfts_record["persons"] = [x["label"] for x in record["persons"]]
+
+    record["places"] = []
+    for y in doc.any_xpath(".//tei:back//tei:place[@xml:id]"):
+        item = {"id": get_xmlid(y), "label": make_entity_label(y.xpath("./*[1]")[0])[0]}
+        record["places"].append(item)
+    cfts_record["places"] = [x["label"] for x in record["places"]]
+
+    record["orgs"] = []
+    for y in doc.any_xpath(".//tei:back//tei:org[@xml:id]"):
+        item = {"id": get_xmlid(y), "label": make_entity_label(y.xpath("./*[1]")[0])[0]}
+        record["orgs"].append(item)
+
+    record["works"] = []
+    for y in doc.any_xpath(".//tei:back//tei:bibl[@xml:id]"):
+        item = {
+            "id": get_xmlid(y),
+            "label": extract_fulltext(y),
+        }
+        record["works"].append(item)
+    cfts_record["places"] = [x["label"] for x in record["places"]]
+    record["full_text"] = f"{extract_fulltext(body)} {record['title']}"
+    cfts_record["full_text"] = record["full_text"]
     records.append(record)
     cfts_records.append(cfts_record)
 
-make_index = client.collections['schnitzler-briefe'].documents.import_(records)
+make_index = client.collections[COLLECTION_NAME].documents.import_(records)
 print(make_index)
-print('done with indexing schnitzler-briefe')
+print(f"done with indexing {COLLECTION_NAME}")
 
-make_index = CFTS_COLLECTION.documents.import_(cfts_records, {'action': 'upsert'})
+make_index = CFTS_COLLECTION.documents.import_(cfts_records, {"action": "upsert"})
 print(make_index)
-print('done with cfts-index schnitzler-briefe')
+print(f"done with cfts-index {COLLECTION_NAME}")
