a115d03f3cb65e96affd263d901ac4927b1a9a10
Peter Andorfer <peter.andorfer@oeaw.ac.at>
2024-09-20 11:19:36 +0200

    Update arche__ingest_binaries.sh

--- arche/arche__ingest_binaries.sh
+++ arche/arche__ingest_binaries.sh
@@ -6,4 +6,4 @@ docker run --rm \
   --network="host" \
   --entrypoint arche-import-binary \
   acdhch/arche-ingest \
-  /data ${TOPCOLID}/ ${ARCHE} ${ARCHE_USER} ${ARCHE_PASSWORD} --skip not_exist
+  /data ${TOPCOLID}/ ${ARCHE} ${ARCHE_USER} ${ARCHE_PASSWORD} --versioning digest --skip not_exist
