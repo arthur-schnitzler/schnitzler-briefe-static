163f82f1ed33ad1ea1e3d30cd37248fa80d5d99d
martinantonmueller <martin.anton.mueller@oeaw.ac.at>
2025-11-10 09:53:27 +0100

    Anpassung

--- shellscripts/script.sh
+++ shellscripts/script.sh
@@ -3,7 +3,7 @@
 # hacky way to make shure the script
 # gets allways run from parent-dir
 # so relative paths get resolved the righ way
-script_location_dir="${BASH_SOURCE[0]}"
-cd $script_location_dir && cd ..
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$script_dir/.."
 # run scripts
 ./shellscripts/dl_imprint.sh
