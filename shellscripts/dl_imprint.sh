965c5e1f13730da9a8541411802668f734b703d7
martinantonmueller <martin.anton.mueller@oeaw.ac.at>
2025-11-10 09:56:02 +0100

    nochmals anpassung notwendig

--- shellscripts/dl_imprint.sh
+++ shellscripts/dl_imprint.sh
@@ -2,8 +2,9 @@
 
 REDMINE_ID=21435
 IMPRINT_XML=./data/imprint.xml
-rm ${IMPRINT_XML}
-echo '<?xml version="1.0" encoding="UTF-8"?>'
+mkdir -p ./data
+rm -f ${IMPRINT_XML}
+echo '<?xml version="1.0" encoding="UTF-8"?>' > ${IMPRINT_XML}
 echo "<root>" >> ${IMPRINT_XML}
 curl https://shared.acdh.oeaw.ac.at/acdh-common-assets/api/imprint.php?serviceID=${REDMINE_ID} >> ${IMPRINT_XML}
 echo "</root>" >> ${IMPRINT_XML}
